c85cafd1bc6e4c4f4402a300135f74ce68bb4426
Sudharshan <sudharshan19@iiserb.ac.in>
2024-10-28 09:56:56 +0530

    Github pages commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sudharshan's <code>React</code> app hosted on Github pages!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from the docs..
         </a>
       </header>
     </div>
